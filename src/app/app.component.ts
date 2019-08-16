97ddbbb28c0596fa47c907b3176d2b0c5847e2ff
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-08-16 17:52:56 +0300

    select control validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,14 @@ import { User } from './models/user';
 })
 export class AppComponent {
   topics = ['Angular', 'React', 'Vue'];
-  userModel = new User('', 'rob@test.com', '5556665566', 'React', 'evening', true);
+  userModel = new User('', 'rob@test.com', '5556665566', 'default', 'evening', true);
+
+  topicHasError = true;
+  validateTopic(val: string) {
+    if (val === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
 }
