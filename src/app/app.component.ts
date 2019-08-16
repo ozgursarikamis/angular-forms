9b20ea49f16f4c33b8e505b5498ced9ae9f19dbf
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-08-17 01:35:18 +0300

    created reacted form model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { User } from './models/user';
-import { EnrollmentService } from './services/enrollment.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,28 +7,9 @@ import { EnrollmentService } from './services/enrollment.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  topics = ['Angular', 'React', 'Vue'];
-  userModel = new User('', 'rob@test.com', '5556665566', 'default', 'evening', true);
-  submitted = false;
-  errorMessage = '';
-
-  constructor(private enrollmentservice: EnrollmentService) { }
-  topicHasError = true;
-  validateTopic(val: string) {
-    if (val === 'default') {
-      this.topicHasError = true;
-    } else {
-      this.topicHasError = false;
-    }
-  }
-
-  onSubmit(userForm) {
-    console.log('userForm :', userForm);
-    this.submitted = true;
-    this.enrollmentservice.enroll(this.userModel)
-      .subscribe(
-        data => { console.log('Post Success:', data); },
-        error => { this.errorMessage = error.statusText; }
-      );
-  }
+  registrationForm = new FormGroup({
+    username: new FormControl('Vishwas'),
+    password: new FormControl('123456'),
+    confirmPassword: new FormControl('123456')
+  });
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
@@ -15,7 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
