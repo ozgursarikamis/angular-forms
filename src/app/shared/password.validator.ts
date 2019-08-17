74e8b4f7cea33c85107f06681d810c8abb663ce9
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-08-17 03:22:49 +0300

    form control is pristine?

--- src/app/shared/password.validator.ts
+++ src/app/shared/password.validator.ts
@@ -3,6 +3,11 @@ import { AbstractControl } from '@angular/forms';
 export function passwordValidator(control: AbstractControl): {[key: string]: boolean} | null {
     const password = control.get('password');
     const confirmPassword = control.get('confirmPassword');
+
+    if (password.pristine || confirmPassword.pristine) {
+        return null; // null: there is no error!
+    }
+
     return password
         && confirmPassword
         && password.value !== confirmPassword.value ? { mismatchedPasswords: true } : null;
