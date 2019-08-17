9002fc0305505f641305b9608eb8ff09d3e5150d
ÖZGÜR SARIKAMIŞ <ozgursarikamis@gmail.com>
2019-08-17 03:20:42 +0300

    cross validations on different form items: password match control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { forbiddenUsernamesValidator } from './shared/username.validator';
+import { passwordValidator } from './shared/password.validator';
 
 @Component({
   selector: 'app-root',
@@ -21,7 +22,8 @@ export class AppComponent {
     address: this.formBuilder.group({
       city: ['Van'], state: [''], postalCode: ['']
     })
-  });
+  }, { validator: [passwordValidator] }); // passwordValidator is validating the group!
+
   constructor(private formBuilder: FormBuilder) { }
 
   get username() {
